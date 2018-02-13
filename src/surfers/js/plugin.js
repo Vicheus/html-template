924c71cd20bd5bdaaa99a028c9c407e8a739b227
Olexandr Kononchuk <okononchuk@grossum.com>
2018-02-13 10:50:19 +0200

    developed section about us

--- src/surfers/js/plugin.js
+++ src/surfers/js/plugin.js
@@ -9,4 +9,28 @@ document.addEventListener('DOMContentLoaded', function(){
     menu.setAttribute('style', `left: ${parentOffset.left - (menuOffset.width / 2) + (parentOffset.width / 2)}px`);
     e.preventDefault();
   });
+  let div, n,
+    v = document.getElementsByClassName("s__about_us__content__video");
+  for (n = 0; n < v.length; n++) {
+    div = document.createElement("div");
+    div.setAttribute("data-id", v[n].dataset.id);
+    div.innerHTML = labnolThumb(v[n].dataset.id);
+    // div.onclick = labnolIframe; // uncomment for playing youtube video
+    v[n].appendChild(div);
+  }
 }, false);
+
+function labnolThumb(id) {
+  const thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
+    play = '<div class="play"></div>';
+  return thumb.replace("ID", id) + play;
+}
+
+function labnolIframe() {
+  const iframe = document.createElement("iframe");
+  const embed = "https://www.youtube.com/embed/ID?autoplay=1";
+  iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
+  iframe.setAttribute("frameborder", "0");
+  iframe.setAttribute("allowfullscreen", "1");
+  this.parentNode.replaceChild(iframe, this);
+}
